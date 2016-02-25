c1c31136c091e212512da4c1227917bb99ff2f6f
JacksonLLee <jsllee.phon@gmail.com>
2016-02-24 18:53:36 -0600

    break down test_words_to_neighbors() for each word

--- tests/test_manifold.py
+++ tests/test_manifold.py
@@ -13,7 +13,9 @@ def test_words_to_neighbors():
 
     expected_object_path = os.path.join(data_dir, 'words_to_neighbors.txt')
     expected_object = eval(open(expected_object_path).read())
-    assert test_object == expected_object
+
+    for word in test_object.keys():
+        assert test_object[word] == expected_object[word]
 
 
 def test_words_to_contexts():
