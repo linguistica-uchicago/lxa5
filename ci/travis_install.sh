fd636f4c4ae30b8ca75d54b44f8889ca8549d73d
JacksonLLee <jsllee.phon@gmail.com>
2016-03-08 17:54:21 -0600

    Remove command "deactivate" in travis_install.sh

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -7,10 +7,6 @@ set -e
 # The script is to install all dependencies for both the Linguistica library
 # and its tests.
 
-# Deactivate the travis-provided virtual environment and setup a
-# conda-based environment instead
-deactivate
-
 # Use the miniconda installer for faster download / install of conda
 # itself
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
