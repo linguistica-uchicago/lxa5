8c345b28e54adecebffbd1dfd0cff413a40bf137
JacksonLLee <jsllee.phon@gmail.com>
2016-03-08 18:31:36 -0600

    Remove command "deactivate" in travis_install.sh

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -14,10 +14,6 @@ set -e
 export CC=gcc
 export CXX=g++
 
-# Deactivate the travis-provided virtual environment and setup a
-# conda-based environment instead
-deactivate
-
 # Use the miniconda installer for faster download / install of conda
 # itself
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
