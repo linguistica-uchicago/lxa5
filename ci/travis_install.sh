08013a521eb0515639cacb79afb8389f09fb581f
JacksonLLee <jsllee.phon@gmail.com>
2016-03-08 18:59:45 -0600

    Fix travis_install.sh by conda's documentation

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# modified from https://github.com/rhiever/tpot
+# based on:
+# - https://github.com/rhiever/tpot
+# - http://conda.pydata.org/docs/travis.html
 
 # This script is called by the "install" step defined in
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
@@ -18,11 +20,15 @@ export CXX=g++
 # itself
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     -O miniconda.sh
-chmod +x miniconda.sh && ./miniconda.sh -b
-export PATH=/home/travis/miniconda/bin:$PATH
-conda update --yes conda
+chmod +x miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
 
-conda create -n testenv --yes python=$PYTHON_VERSION numpy scipy networkx
+conda create -q -n testenv --yes python=$PYTHON_VERSION numpy scipy networkx
 
 source activate testenv
 
@@ -30,4 +36,8 @@ source activate testenv
 python -m pip install python-coveralls coverage nose
 
 # Install Linguistica
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import networkx; print('networkx %s' % networkx.__version__)"
 python setup.py install
