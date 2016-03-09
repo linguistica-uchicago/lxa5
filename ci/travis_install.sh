f6b112d257a967ca35df975f111e6a3b741876ba
JacksonLLee <jsllee.phon@gmail.com>
2016-03-08 18:29:18 -0600

    Fix pip install error

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,26 +1,37 @@
-#!/bin/env bash
+#!/bin/bash
 
-set -e
+# modified from https://github.com/rhiever/tpot
 
 # This script is called by the "install" step defined in
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
 # The script is to install all dependencies for both the Linguistica library
 # and its tests.
 
+set -e
+
+# Fix the compilers to workaround avoid having the Python 3.4 build
+# lookup for g++44 unexpectedly.
+export CC=gcc
+export CXX=g++
+
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead
+deactivate
+
 # Use the miniconda installer for faster download / install of conda
 # itself
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy networkx
+chmod +x miniconda.sh && ./miniconda.sh -b
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
 
-source activate test-environment
-which python
+conda create -n testenv --yes python=$PYTHON_VERSION numpy scipy networkx
 
-# Install packages needed for running tests and measuring test coverage
-python -m pip python-coveralls coverage nose
+source activate testenv
+
+# Install packages for running tests and measuring test coverage
+python -m pip install python-coveralls coverage nose
+
+# Install Linguistica
+python setup.py install
