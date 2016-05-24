3b060652fc9eabdff5ecc37ff1e58d65badcba0d
JacksonLLee <jsllee.phon@gmail.com>
2016-05-24 14:41:56 -0500

    Create new module "datasets" with brown and cmudict
    english-brown.txt and english-cmudict.dx1 are included in the directory "datasets".
    ``brown`` and ``cmudict`` are defined as the respective file paths.

---
+++ linguistica/datasets/__init__.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf8 -*-
+
+import os
+
+brown = os.path.join(os.path.dirname(__file__), 'english-brown.txt')
+cmudict = os.path.join(os.path.dirname(__file__), 'english-cmudict.dx1')
